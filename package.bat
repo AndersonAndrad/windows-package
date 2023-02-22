7130049cc69ec7aaa326c004feda49ae3bcf588e
AndersonAndrad <anderson_andrade_@outlook.com>
2023-02-22 01:32:26 -0300

    refactor: comments some softwares in list
    this softwares are removed because is not necessary in my day, when is necessary remove commentary and execute script

--- package.bat
+++ package.bat
@@ -1,44 +1,43 @@
 @ECHO OFF
 
 :: Run this script with administrator
-:: Install chocolatey in PC
+:: Install chocolatey
+:: @TODO - Test if run installing chocolatey and try install softwares in the same script
+:: @TODO - in case it doesn't work create two scripts  first install chocolatey and call the script to install all softwares
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 :: Install all softwares below
 :: If you want insert or remove any software in this script insert or remove below
 :: Search in https://community.chocolatey.org/packages you software, some software can be change name to install
 choco install ^
-:: Android studio 
-virtualbox ^
-genymotion ^
-androidstudio ^
-android-sdk^
-:: Docker
-docker-cli ^
-docker-desktop ^
-:: Programmer
+:: - Android studio 
+:: virtualbox ^ 
+:: genymotion ^ 
+:: androidstudio ^
+:: android-sdk^
+:: - Docker
+:: docker-cli ^
+:: docker-desktop ^
+:: - Programmer
 vscode ^
 git ^
 insomnia-rest-api-client ^
 nodejs-lts ^
 wsl2 ^
-yarn ^
-github-desktop ^
-:: Communication
+:: - Communication
 discord  ^
 telegram ^
 whatsapp ^
-:: Font
+:: - Font
 firacode ^
 jetbrainsmono ^
-:: Games
+:: - Games
 steam ^
 epicgameslauncher ^
-:: Browser
+:: - Browser
 googlechrome ^
-:: Others
+:: - Others
 bitwarden ^
-figma ^
 javaruntime ^
 lightshot.install ^
 vlc ^
